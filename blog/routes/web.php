a9bc12686a356ca52d74b41e69ab90078994ce6f
radu-chira <radu.chira@plentydevelopment.com>
2019-06-25 10:16:03 +0300

    rename page controller

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,28 +11,6 @@
 |
 */
 
-Route::get('/', 'PageController@home');
-Route::get('/contact', 'PageController@contact');
-Route::get('/about', 'PageController@about');
-
-//Route::get('/', function () {
-//    $tasks = [
-//        'go to the store',
-//        'go to the market',
-//        'go to work',
-//        'go to concert'
-//    ];
-//    //return view('welcome')->withTasks($tasks)->withUser('user');
-//    return view('welcome', ['tasks' => $tasks, 'user' => 'tstUser']);
-//});
-
-
-//Route::get('/contact', function () {
-//    return view('contact');
-//});
-
-
-//Route::get('/about', function () {
-//    return view('about');
-//});
-
+Route::get('/', 'PagesController@home');
+Route::get('/contact', 'PagesController@contact');
+Route::get('/about', 'PagesController@about');
